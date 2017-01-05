69bbbd060d6fbe48258b505b4935c48ca47a193a
Drew Vollrath <andre@vollrath.ca>
2017-01-05 23:15:16 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  def initialize(side_1,side_2,side_3)
+    @side = [side_1,side_2,side_3]
+  end
+
+  def kind
+    side.sort!
+    if invalid?
+      raise TriangleError
+    else
+      type
+    end
+  end
+
+  private
+  def side
+    @side
+  end
+
+  def type
+    case side.uniq.count
+    when 1
+      :equilateral
+    when 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def invalid?
+    side[0]+side[1] <= side[2] || side.inject(:+) == 0 ? true : false
+  end
+end
+
+class TriangleError < StandardError
 end
